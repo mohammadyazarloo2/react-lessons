87d6b13f363c974d0f8aafb477c3f00570623e20
mohammadyazarloo2 <mohammadyazarloo2@gmail.com>
2020-02-10 02:20:20 +0330

    begining functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Greate from './components/greate'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        hello world
+        <Greate />
       </div>
     );
   }
---
+++ src/components/greate.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Greate=()=><h1>hello wishwas</h1>
+
+export default Greate
