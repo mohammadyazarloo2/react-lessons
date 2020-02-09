8466c014beeb4fa81e61181381412f2431a996d6
mohammadyazarloo2 <mohammadyazarloo2@gmail.com>
2020-02-10 03:01:57 +0330

    begining class components

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Greate from './components/greate'
+import Welcome from './components/welcome'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Greate />
+        <Welcome />
       </div>
     );
   }
