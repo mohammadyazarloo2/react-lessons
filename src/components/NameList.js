1cb48fece95f8b361adf9677cc02448967808e67
mohammadyazarloo2 <mohammadyazarloo2@gmail.com>
2020-03-01 23:48:05 +0330

    index as key anti-patter

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Person from './Person'
 
 function NameList(){
+    const names=['Bruce','Clark','Diana','Bruce']
     const persons=[
         {
             id:1,
@@ -22,12 +23,12 @@ function NameList(){
             skill:'vue'
         }
     ]
-const personsList=persons.map(person =><Person key={person.id} person={person} />)
+const nameList=names.map((name,index) =><h2 key={index}>{index} {name}</h2>)
 
     return(
         <div>
             {
-                personsList
+                nameList
             }
         </div>
     )
